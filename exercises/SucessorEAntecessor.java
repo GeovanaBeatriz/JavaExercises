c3bda5717861595669976ef1ee8972a472f9ee51
GeovanaBeatriz <geovana.jesus.184945@gmail.com>
2023-03-17 14:26:01 -0300

    Adjusting exercises - SucessorEAntecessor

--- exercises/SucessorEAntecessor.java
+++ exercises/SucessorEAntecessor.java
@@ -6,18 +6,10 @@ public class SucessorEAntecessor {
 
         int numero = Integer.parseInt(JOptionPane.showInputDialog("Digite um numero: "));
 
-        if (numero < 0){
-            int sucessor = numero - 1;
-            int antecessor = numero + 1;
-
-            JOptionPane.showMessageDialog(null, "O sucessor de " + numero + " e " + sucessor + " e seu antecessor e " + antecessor);
-        }
-        else{
-
+        
             int sucessor = numero + 1;
             int antecessor = numero - 1;
 
             JOptionPane.showMessageDialog(null, "O sucessor de " + numero + " e " + sucessor + " e seu antecessor e " + antecessor);
-        }
     }
 }
--- exercises/SucessorEAntecessor2.java
+++ exercises/SucessorEAntecessor2.java
@@ -6,22 +6,13 @@ public class SucessorEAntecessor2 {
 
         int numero = Integer.parseInt(JOptionPane.showInputDialog("Digite um numero: "));
 
-        if (numero < 0){
-            int sucTriplo = numero * 3 - 1;
-            int antDobro = numero * 2 + 1;
-
-            int soma = sucTriplo + antDobro;
-
-            JOptionPane.showMessageDialog(null, "O sucessor do triplo " + numero + " e " + sucTriplo + " e seu antecessor de seu dobro e " + antDobro + ". Sua soma e " + soma);
-        }
-        else{
-
+        
             int sucTriplo = numero * 3 + 1;
             int antDobro = numero * 2 - 1;
 
             int soma = sucTriplo + antDobro;
-            
+
             JOptionPane.showMessageDialog(null, "O sucessor do triplo " + numero + " e " + sucTriplo + " e seu antecessor de seu dobro e " + antDobro + ". Sua soma e " + soma);
-        }
+        
     }
 }
