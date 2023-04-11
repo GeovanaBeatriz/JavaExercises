3ba0de536ff8a2e5941681c7d75b9d45a8d4e04f
GeovanaBeatriz <geovana.jesus.184945@gmail.com>
2023-04-11 12:51:25 -0300

    Tratamento de exceção e busca de categoria por id

--- dscatalogBootcamp/dscatalogBootcamp/src/main/java/com/geovanabeatriz/dscatalogBootcamp/services/CategoryService.java
+++ dscatalogBootcamp/dscatalogBootcamp/src/main/java/com/geovanabeatriz/dscatalogBootcamp/services/CategoryService.java
@@ -2,10 +2,13 @@ package com.geovanabeatriz.dscatalogBootcamp.services;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Optional;
+
+import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.geovanabeatriz.dscatalogBootcamp.dto.CategoryDTO;
 import com.geovanabeatriz.dscatalogBootcamp.entities.Category;
@@ -17,6 +20,7 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository repository;
 	
+	@Transactional(readOnly = true)
 	public List<CategoryDTO> findAll(){
 		List<Category> list = repository.findAll();
 		
@@ -29,5 +33,12 @@ public class CategoryService {
 		}
 		
 		return listDto;
+	}
+
+	@Transactional(readOnly = true)
+	public CategoryDTO findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+		return new CategoryDTO(entity);
 	}
 }
