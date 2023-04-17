b836e1f23a844ef52b95830cd728718611c1a02f
GeovanaBeatriz <geovana.jesus.184945@gmail.com>
2023-04-17 12:12:02 -0300

    Inserindo nova categoria com POST

--- exercises/Repeticao/NumerosNaturais.java
+++ exercises/Repeticao/NumerosNaturais.java
@@ -6,10 +6,11 @@ public class NumerosNaturais {
     
     public static void main(String[] args) {
         
-        Scanner scanner= new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
         System.out.println("Digite um número inteiro: ");
-        int num = scanner.nextInt();
+       
 
+        scanner.close();
     }
 }
