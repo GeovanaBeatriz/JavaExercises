da5341d30b92543dc5c6850a0bd347bfebf33a16
GeovanaBeatriz <geovana.jesus.184945@gmail.com>
2023-04-17 13:12:49 -0300

    Dados de auditoria

--- dscatalogBootcamp/dscatalogBootcamp/src/main/java/com/geovanabeatriz/dscatalogBootcamp/resources/exceptions/ResourceExceptionHandler.java
+++ dscatalogBootcamp/dscatalogBootcamp/src/main/java/com/geovanabeatriz/dscatalogBootcamp/resources/exceptions/ResourceExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.geovanabeatriz.dscatalogBootcamp.services.exceptions.DatabaseException;
 import com.geovanabeatriz.dscatalogBootcamp.services.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice
@@ -24,5 +25,17 @@ public class ResourceExceptionHandler {
 		err.setMessage(e.getMessage());
 		err.setPath(request.getRequestURI());
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+	}
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request){
+		
+		StandardError err = new StandardError();
+		err.setTimestamp(Instant.now());
+		err.setStatus(HttpStatus.BAD_REQUEST.value());
+		err.setError("Database Exception");
+		err.setMessage(e.getMessage());
+		err.setPath(request.getRequestURI());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
 	}
 }
--- dscatalogBootcamp/dscatalogBootcamp/src/main/java/com/geovanabeatriz/dscatalogBootcamp/services/CategoryService.java
+++ dscatalogBootcamp/dscatalogBootcamp/src/main/java/com/geovanabeatriz/dscatalogBootcamp/services/CategoryService.java
@@ -7,12 +7,15 @@ import java.util.Optional;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.geovanabeatriz.dscatalogBootcamp.dto.CategoryDTO;
 import com.geovanabeatriz.dscatalogBootcamp.entities.Category;
 import com.geovanabeatriz.dscatalogBootcamp.repositories.CategoryRepository;
+import com.geovanabeatriz.dscatalogBootcamp.services.exceptions.DatabaseException;
 import com.geovanabeatriz.dscatalogBootcamp.services.exceptions.ResourceNotFoundException;
 
 @Service //Service realiza a lógica da aplicação
@@ -61,6 +64,18 @@ public class CategoryService {
 		}
 		catch(EntityNotFoundException e) {
 			throw new ResourceNotFoundException("id not found");
+		}
+	}
+
+	public void delete(Long id) {
+		try {
+		repository.deleteById(id);
+		}
+		catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found " + id); 
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation"); 
 		}
 	}
 }
