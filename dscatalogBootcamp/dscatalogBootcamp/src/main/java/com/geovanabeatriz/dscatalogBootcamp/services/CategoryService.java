ca89458af38a3a1abdf400e7f9580bd40c223112
Geovana Beatriz <geovana.jesus.184945@gmail.com>
2023-04-18 20:49:40 -0300

    Paginacao

--- dscatalogBootcamp/dscatalogBootcamp/src/main/java/com/geovanabeatriz/dscatalogBootcamp/services/CategoryService.java
+++ dscatalogBootcamp/dscatalogBootcamp/src/main/java/com/geovanabeatriz/dscatalogBootcamp/services/CategoryService.java
@@ -9,6 +9,8 @@ import javax.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -25,18 +27,21 @@ public class CategoryService {
 	private CategoryRepository repository;
 	
 	@Transactional(readOnly = true)
-	public List<CategoryDTO> findAll(){
-		List<Category> list = repository.findAll();
+	public Page<CategoryDTO> findAllPaged(PageRequest pageRequest){
+		Page<Category> list = repository.findAll(pageRequest);
 		
 		//Faz a mesma coisa que o bloco abaixo
-		// List<CategoryDTO> listDto = list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+		//List<CategoryDTO> listDto = list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 		
-		List<CategoryDTO> listDto = new ArrayList<>();
+		/*List<CategoryDTO> listDto = new ArrayList<>();
 		for (Category cat : list) {
 			listDto.add(new CategoryDTO(cat));
 		}
 		
 		return listDto;
+		*/
+		
+		return list.map(x -> new CategoryDTO(x));
 	}
 
 	@Transactional(readOnly = true)
