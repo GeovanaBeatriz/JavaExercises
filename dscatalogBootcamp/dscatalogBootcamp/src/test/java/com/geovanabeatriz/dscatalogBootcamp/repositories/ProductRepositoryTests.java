0907e71aafc0bead40987725c9693217f93e294e
Geovana Beatriz <geovana.jesus.184945@gmail.com>
2023-04-27 12:50:09 -0300

    second test on repository

--- dscatalogBootcamp/dscatalogBootcamp/src/test/java/com/geovanabeatriz/dscatalogBootcamp/repositories/ProductRepositoryTests.java
+++ dscatalogBootcamp/dscatalogBootcamp/src/test/java/com/geovanabeatriz/dscatalogBootcamp/repositories/ProductRepositoryTests.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.EmptyResultDataAccessException;
 
 import com.geovanabeatriz.dscatalogBootcamp.entities.Product;
 
@@ -25,6 +26,16 @@ public class ProductRepositoryTests {
 		
 		Optional<Product> result = repository.findById(existingId);
 		Assertions.assertFalse(result.isPresent());
+	}
+	
+	@Test
+	public void deleteShouldThrowEmptyResultDataAccessExceptionWhenIdDoesNotExist() {
+		
+		Long nonExistingId = 1000L;
+		
+		Assertions.assertThrows(EmptyResultDataAccessException.class, () -> {
+			repository.deleteById(nonExistingId);
+		});
 	}
 	
 }
