57a8d689f866481331f6775a9e72991f3fc97b2c
Geovana Beatriz <geovana.jesus.184945@gmail.com>
2023-05-01 13:39:12 -0300

    Teste delete lança DatabaseException quando id dependente

--- dscatalogBootcamp/dscatalogBootcamp/src/test/java/com/geovanabeatriz/dscatalogBootcamp/services/ProductServiceTests.java
+++ dscatalogBootcamp/dscatalogBootcamp/src/test/java/com/geovanabeatriz/dscatalogBootcamp/services/ProductServiceTests.java
@@ -7,10 +7,12 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.geovanabeatriz.dscatalogBootcamp.repositories.ProductRepository;
+import com.geovanabeatriz.dscatalogBootcamp.services.exceptions.DatabaseException;
 import com.geovanabeatriz.dscatalogBootcamp.services.exceptions.ResourceNotFoundException;
 
 @ExtendWith(SpringExtension.class) //Teste Unitário
@@ -24,16 +26,32 @@ public class ProductServiceTests {
 	
 	private Long existingId;
 	private Long nonExistingId;
+	private Long dependentId;
 	
 	@BeforeEach
 	void setUp() throws Exception{
 		
 		existingId = 1L;
 		nonExistingId = 1000L;
+		dependentId = 4L;
 		
 		Mockito.doNothing().when(repository).deleteById(existingId);//Aqui diz como o Mock vai agir
 		
 		Mockito.doThrow(EmptyResultDataAccessException.class).when(repository).deleteById(nonExistingId);
+		
+		Mockito.doThrow(DataIntegrityViolationException.class).when(repository).deleteById(dependentId);
+
+	}
+	
+	@Test
+	public void deleteShouldThrowDatabaseExceptionWhenDependentId() {
+		
+		Assertions.assertThrows(DatabaseException.class, () -> {
+			
+			service.delete(dependentId);
+		});
+		
+		Mockito.verify(repository, Mockito.times(1)).deleteById(dependentId); 
 	}
 	
 	@Test
