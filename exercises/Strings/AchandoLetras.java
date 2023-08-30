4a89a6e0596638db8b34b971bacb5aa46a30163b
Suporte-01 <suporte01@ppmike.local>
2023-08-30 10:19:36 -0300

    Substituindo a vogal A na String

---
+++ exercises/Strings/AchandoLetras.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+
+public class AchandoLetras{
+	
+	public static void main(String[] args){
+		
+		Scanner sc = new Scanner(System.in);
+		
+		
+		System.out.print("Digite um nome: ");
+		String nome = sc.next();
+		String minusculas = nome.toLowerCase();
+		
+		
+		for(int x = 0; x < nome.length(); x++){
+			
+			minusculas = minusculas.replaceAll("a", "*");
+		}
+		
+		System.out.print(minusculas);
+		sc.close();
+		
+	}
+}
